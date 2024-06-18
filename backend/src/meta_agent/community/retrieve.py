b8d885a133bf139fe6c99c19f1a9162a44347ef8
orbaker <107270698+dorbaker@users.noreply.github.com>
2024-06-18 13:03:20 -0400

    Add cognitive services endpoint to config parameters (#6)
    Co-authored-by: Josh Bradley <joshbradley@microsoft.com>

--- backend/src/meta_agent/community/retrieve.py
+++ backend/src/meta_agent/community/retrieve.py
@@ -44,7 +44,8 @@ from graphrag.vector_stores.base import (
 )
 
 cognitive_services_endpoint = os.environ.get(
-    "COGNITIVE_SERVICES_ENDPOINT", "https://cognitiveservices.azure.com/.default"
+    "GRAPHRAG_COGNITIVE_SERVICES_ENDPOINT",
+    "https://cognitiveservices.azure.com/.default",
 )
 token_provider = get_bearer_token_provider(
     DefaultAzureCredential(), cognitive_services_endpoint
