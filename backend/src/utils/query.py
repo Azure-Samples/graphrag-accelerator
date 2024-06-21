058da6cfb475b603840863256b623f2b5bb769c4
dorbaker <dorbaker@microsoft.com>
2024-06-21 13:19:09 -0400

    Cleaned up code.

--- backend/src/utils/query.py
+++ backend/src/utils/query.py
@@ -13,6 +13,11 @@ from graphrag.query.indexer_adapters import (
 
 from src.api.azure_clients import BlobServiceClientSingleton
 
+storage_options={
+    "account_name": BlobServiceClientSingleton.get_storage_account_name(),
+    "account_host": BlobServiceClientSingleton.get_instance().url.split("//")[1],
+    "credential": DefaultAzureCredential(),
+}
 
 def get_entities(
     entity_table_path: str,
@@ -21,19 +26,11 @@ def get_entities(
 ) -> pd.DataFrame:
     entity_df = pd.read_parquet(
         entity_table_path,
-        storage_options={
-            "account_name": "null",#BlobServiceClientSingleton.get_storage_account_name(),
-            "account_host": BlobServiceClientSingleton.get_instance().url.split("//")[1],
-            "credential": DefaultAzureCredential(),
-        },
+        storage_options=storage_options,
     )
     entity_embedding_df = pd.read_parquet(
         entity_embedding_table_path,
-        storage_options={
-            "account_name": "null",#BlobServiceClientSingleton.get_storage_account_name(),
-            "account_host": BlobServiceClientSingleton.get_instance().url.split("//")[1],
-            "credential": DefaultAzureCredential(),
-        },
+        storage_options=storage_options,
     )
     return pd.DataFrame(
         read_indexer_entities(entity_df, entity_embedding_df, community_level)
@@ -45,19 +42,11 @@ def get_reports(
 ) -> pd.DataFrame:
     entity_df = pd.read_parquet(
         entity_table_path,
-        storage_options={
-            "account_name": "null",#BlobServiceClientSingleton.get_storage_account_name(),
-            "account_host": BlobServiceClientSingleton.get_instance().url.split("//")[1],
-            "credential": DefaultAzureCredential(),
-        },
+        storage_options=storage_options,
     )
     report_df = pd.read_parquet(
         community_report_table_path,
-        storage_options={
-            "account_name": "null",#BlobServiceClientSingleton.get_storage_account_name(),
-            "account_host": BlobServiceClientSingleton.get_instance().url.split("//")[1],
-            "credential": DefaultAzureCredential(),
-        },
+        storage_options=storage_options,
     )
     return pd.DataFrame(read_indexer_reports(report_df, entity_df, community_level))
 
@@ -65,11 +54,7 @@ def get_reports(
 def get_relationships(relationships_table_path: str) -> pd.DataFrame:
     relationship_df = pd.read_parquet(
         relationships_table_path,
-        storage_options={
-            "account_name": "null",#BlobServiceClientSingleton.get_storage_account_name(),
-            "account_host": BlobServiceClientSingleton.get_instance().url.split("//")[1],
-            "credential": DefaultAzureCredential(),
-        },
+        storage_options=storage_options,
     )
     return pd.DataFrame(read_indexer_relationships(relationship_df))
 
@@ -77,11 +62,7 @@ def get_relationships(relationships_table_path: str) -> pd.DataFrame:
 def get_covariates(covariate_table_path: str) -> pd.DataFrame:
     covariate_df = pd.read_parquet(
         covariate_table_path,
-        storage_options={
-            "account_name": "null",#BlobServiceClientSingleton.get_storage_account_name(),
-            "account_host": BlobServiceClientSingleton.get_instance().url.split("//")[1],
-            "credential": DefaultAzureCredential(),
-        },
+        storage_options=storage_options,
     )
     return pd.DataFrame(read_indexer_covariates(covariate_df))
 
@@ -89,10 +70,6 @@ def get_covariates(covariate_table_path: str) -> pd.DataFrame:
 def get_text_units(text_unit_table_path: str) -> pd.DataFrame:
     text_unit_df = pd.read_parquet(
         text_unit_table_path,
-        storage_options={
-            "account_name": "null",#BlobServiceClientSingleton.get_storage_account_name(),
-            "account_host": BlobServiceClientSingleton.get_instance().url.split("//")[1],
-            "credential": DefaultAzureCredential(),
-        },
+        storage_options=storage_options,
     )
     return pd.DataFrame(read_indexer_text_units(text_unit_df))
