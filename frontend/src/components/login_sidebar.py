eb932012dd02a7e3417e1d13eee6a2639794394e
Josh Bradley <joshbradley@microsoft.com>
2024-07-09 17:40:23 -0400

    add license header

--- frontend/src/components/login_sidebar.py
+++ frontend/src/components/login_sidebar.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 import streamlit as st
 
 from src.enums import EnvVars
--- frontend/src/components/query.py
+++ frontend/src/components/query.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 import json
 from typing import Literal
 
