715fcfad8127602d16558a0a7c1b77e8080f2aa8
Ed Clark <107473135+clarked-msft@users.noreply.github.com>
2024-09-13 02:24:26 -0400

    Use AKS_NAMESPACE env var when creating indexing manager (#145)
    Co-authored-by: Josh Bradley <joshbradley@microsoft.com>

--- backend/src/main.py
+++ backend/src/main.py
@@ -68,11 +68,11 @@ async def lifespan(app: FastAPI):
         ] = pod.spec.service_account_name
         # retrieve list of existing cronjobs
         batch_v1 = client.BatchV1Api()
-        namespace_cronjobs = batch_v1.list_namespaced_cron_job(namespace="graphrag")
+        namespace_cronjobs = batch_v1.list_namespaced_cron_job(namespace=os.environ["AKS_NAMESPACE"])
         cronjob_names = [cronjob.metadata.name for cronjob in namespace_cronjobs.items]
         # create cronjob if it does not exist
         if manifest["metadata"]["name"] not in cronjob_names:
-            batch_v1.create_namespaced_cron_job(namespace="graphrag", body=manifest)
+            batch_v1.create_namespaced_cron_job(namespace=os.environ["AKS_NAMESPACE"], body=manifest)
     except Exception as e:
         print("Failed to create graphrag cronjob.")
         reporter = ReporterSingleton().get_instance()
